2fc3c71f396c95137399a4809b71965cf71e9cb7
meinczak <zsttpmanczak@gmail.com>
2025-04-08 14:09:42 +0200

    któraś lekcja

--- src/desktop.ts
+++ src/desktop.ts
@@ -2,8 +2,10 @@ const op1 = document.getElementById("opt1");
 const op2 = document.getElementById("opt2");
 const op3 = document.getElementById("opt3");
 
-function changeOpt (opt: string) {
+function changeOpt (opt: HTMLElement | null) {
     if (opt) {
-        
+        opt.style.color = "white";
     }
 }
+
+changeOpt(op1);
