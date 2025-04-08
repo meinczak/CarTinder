e9fcf9101f02ef51b0334b74a35c931416109e31
meinczak <zsttpmanczak@gmail.com>
2025-04-08 12:14:33 +0200

    progress po 1 lekcji

---
+++ src/desktop.ts
@@ -0,0 +1,9 @@
+const op1 = document.getElementById("opt1");
+const op2 = document.getElementById("opt2");
+const op3 = document.getElementById("opt3");
+
+function changeOpt (opt: string) {
+    if (opt) {
+        
+    }
+}
